5d56f2e669bf3a4f5b2dc72bc4c5aae8709ab89a
David Vasko <vasko6d@gmail.com>
2019-11-25 13:29:00 -0800

    adding kody to my shell script

--- bin/8a-scraper.sh
+++ bin/8a-scraper.sh
@@ -8,7 +8,8 @@ for cmnd in \
     'python3 bin/8a-scraper.py --delimiter "json" --tmpFile scott-baron.html --outFile scott-baron.json "https://www.8a.nu/scorecard/dirk-irector/boulders/?AscentClass=0&AscentListViewType=0&GID=a03bc066b057a4812722a15035a58bad"' \
     'python3 bin/8a-scraper.py --delimiter "json" --tmpFile nathaniel-cushing-murray.html --outFile nathaniel-cushing-murray.json "https://www.8a.nu/scorecard/chris-rush/boulders/?AscentClass=0&AscentListViewType=0&GID=7df805fe7444482ff3cfd8ecd38aaace"' \
     'python3 bin/8a-scraper.py --delimiter "json" --tmpFile drew-gomberg.html --outFile drew-gomberg.json "https://www.8a.nu/scorecard/chris-hoss/boulders/?AscentClass=0&AscentListViewType=0&GID=72bb5f75dad248435df3bf295f1c0964"' \
-    'python3 bin/8a-scraper.py --delimiter "json" --tmpFile akhil-mauze.html --outFile akhil-mauze.json "https://www.8a.nu/scorecard/l-i-b/boulders/?AscentClass=0&AscentListViewType=0&GID=a2ee19aa7a3b855a7714b70831b73e7e"'; \
+    'python3 bin/8a-scraper.py --delimiter "json" --tmpFile akhil-mauze.html --outFile akhil-mauze.json "https://www.8a.nu/scorecard/l-i-b/boulders/?AscentClass=0&AscentListViewType=0&GID=a2ee19aa7a3b855a7714b70831b73e7e"' \
+    'python3 bin/8a-scraper.py --delimiter "json" --tmpFile kody-shutt.html --outFile kody-shutt.json "https://www.8a.nu/scorecard/kody-shutt/boulders/?AscentClass=0&AscentListViewType=0&GID=70e6be69d70480662665cac4533a212b"'; \
     do
     for i in $(seq 1 5); do 
         [ $i -gt 1 ] && sleep 15; 
